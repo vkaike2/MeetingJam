909332d2def1a704acbf375a68418e4fabab3c13
Victor Kaike <vkaike9@gmail.com>
2019-04-27 23:06:38 -0300

    Initial Commit

---
+++ Assets/Code/Cenario/CanoCenario.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanoCenario : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject canvas;
+
+    public void AtivarCanvas()
+    {
+        canvas.SetActive(true);
+    }
+    public void DesativarCanvas()
+    {
+        canvas.SetActive(false);
+    }
+}
---
+++ Assets/Code/Cenario/Pedra.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedra : MonoBehaviour
+{
+    [SerializeField]
+    private List<Sprite> tiposPedra;
+
+    private void Start()
+    {
+        if(tiposPedra.Count > 0)
+        {
+            var SpriteRenderer = this.GetComponent<SpriteRenderer>();
+
+            int posicaoRandomica = Random.Range(0, tiposPedra.Count);
+            Sprite spriteRandomico = tiposPedra[posicaoRandomica];
+            SpriteRenderer.sprite = spriteRandomico;
+        }
+    }
+}
